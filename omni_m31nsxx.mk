98132bf26090e5d752d3a81fd145e26b919ec680
Yilliee <m.taha6f@gmail.com>
2021-03-05 09:01:43 +0000

    Renaming to m31nsxx : renaming things that got left

--- omni_m31nsxx.mk
+++ omni_m31nsxx.mk
@@ -2,14 +2,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Include any options that can't be included in BoardConfig.mk
-$(call inherit-product, device/samsung/a51/device.mk)
+$(call inherit-product, device/samsung/m31nsxx/device.mk)
 
 # Inherit from those products. Most specific first.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_a51nsxx
-PRODUCT_DEVICE := a51nsxx
-PRODUCT_MODEL := SM-A515F
+PRODUCT_NAME := omni_m31nsxx
+PRODUCT_DEVICE := m31nsxx
+PRODUCT_MODEL := SM-M315F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
